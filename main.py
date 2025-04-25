23415061415d0899ff0c981816df0d27af3a0336
ShubhamZoro <shubhamshekhar19@gmail.com>
2025-04-25 14:26:38 +0530

    Learned about path and query parameter

--- main.py
+++ main.py
@@ -16,3 +16,23 @@ def update_item(item_id: int, name: str, price: float):
 @app.delete("/items/{item_id}")
 def delete_item(item_id: int):
     return {"message": f"Item with id {item_id} has been deleted."}
+
+#path parameter
+
+@app.get("user/{user_id}")
+def road_user(user_id: int):
+    return {"user_id": user_id}
+
+#query parameter
+@app.get("/user/")
+def road_user(user_id: int,name: str):
+    return {"user_id": user_id, "name": name}
+
+#path and query parameter
+@app.get("/user/{user_id}/details")
+def road_user_details(user_id: int,include_email: bool = False):
+    if include_email:
+        return {"user_id": user_id, "include email": "email included"}
+    else:
+        return {"user_id": user_id, "include email": "email not included"}
+    
